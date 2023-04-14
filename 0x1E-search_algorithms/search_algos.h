bf6b28ff2ce65bec8479ba5f00d0fbdc3a04cb22
Zico007 <zicogunfolaju@gmail.com>
2023-04-14 01:21:35 +0100

    Completed tasks 0-6

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,12 @@
+#ifndef SEARCH_ALGOS
+#define SEARCH_ALGOS
+#include <stdlib.h>
+#include <stddef.h>
+
+#define MIN(a, b) (((a) > (b)) ? (a) : (b))
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+
+#endif
